c01876a0d1e3fe6d59a6f24b84e98769f6df4264
Masooma14335-tech <syeda.14335.ac@iqra.edu.pk>
2025-12-10 19:30:24 +0500

    analysis.py

--- analysis.py
+++ analysis.py
@@ -1,9 +1,6 @@
 import pandas as pd
-
-# CSV file load karein
 df = pd.read_csv("Student_grades.csv.csv")
 
-# Top 5 rows
 print(df.head())
 
 score_columns = [
@@ -16,10 +13,6 @@ df["Average_Score"] = df[score_columns].mean(axis=1)
 print("\n✔ Average Scores Added!\n")
 print(df[["Student_ID", "Name", "Average_Score"]].head())
 
-# ---------------------------------------------------
-# 3. Find Student With the Highest Average
-# ---------------------------------------------------
-
 top_student = df.loc[df["Average_Score"].idxmax()]
 
 print("\n🎉 STUDENT WITH HIGHEST AVERAGE 🎉")
@@ -29,12 +22,10 @@ print(f"Name: {top_student['Name']}")
 print(f"Average Score: {top_student['Average_Score']:.2f}")
 print("-------------------------------------")
 
-# ---------------------------------------------------
-# 4. Save Updated CSV (Optional)
-# ---------------------------------------------------
 
 output_path = r"C:\Users\YourFolder\students_with_average.csv"
 df.to_csv(output_path, index=False)
 
 print("\n✔ New CSV saved with averages added!")
 print("File:", output_path)
+
