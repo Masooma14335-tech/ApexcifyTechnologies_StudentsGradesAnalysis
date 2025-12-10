5a44bf1396899928c96faa4f530cf732843e3167
Masooma14335-tech <syeda.14335.ac@iqra.edu.pk>
2025-12-10 19:33:27 +0500

    Add files via upload

---
+++ analysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+df = pd.read_csv("Student_grades.csv.csv")
+
+
+print(df.head())
+
+score_columns = [
+    "Maths_Score", "English_Score", "Science_Score",
+    "Computer_Score", "GK_Score", "Statistics_Score"
+]
+
+df["Average_Score"] = df[score_columns].mean(axis=1)
+
+print("\n✔ Average Scores Added!\n")
+print(df[["Student_ID", "Name", "Average_Score"]].head())
+
+
+
+top_student = df.loc[df["Average_Score"].idxmax()]
+
+print("\n🎉 STUDENT WITH HIGHEST AVERAGE 🎉")
+print("-------------------------------------")
+print(f"Student ID: {top_student['Student_ID']}")
+print(f"Name: {top_student['Name']}")
+print(f"Average Score: {top_student['Average_Score']:.2f}")
+print("-------------------------------------")
+
+
+output_path = r"C:\Users\YourFolder\students_with_average.csv"
+df.to_csv(output_path, index=False)
+
+print("\n✔ New CSV saved with averages added!")
+print("File:", output_path)
